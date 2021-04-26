d9782f328301c07afa4d38ad605cf5951e946594
Pc <bmorenor@unbosque.edu.co>
2021-04-26 08:57:23 -0500

    algoritmo listo

--- src/AlgoritmoFloyd.java
+++ src/AlgoritmoFloyd.java
@@ -1,84 +1,81 @@
 public class AlgoritmoFloyd {
 
-        public String floyd(long[][] adyacencia)
-        {
-            int n=adyacencia.length;
-            long D[][]=adyacencia;
-            String resultado ="";
+    public String floyd(long[][] adyacencia)
+    {
+        int n=adyacencia.length;
+        long D[][]=adyacencia;
 
-            String enlaces[][]=new String [n][n];
-            String[][] aux_enlaces=new String[adyacencia.length][adyacencia.length];
+        String enlaces[][]=new String [n][n];
+        String[][] aux_enlaces=new String[adyacencia.length][adyacencia.length];
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                enlaces[i][j]="";
+                aux_enlaces[i][j]="";
+            }
+        }
+        String enl_rec="";
+        for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    enlaces[i][j]="";
-                    aux_enlaces[i][j]="";
-                }
-            }
-            String enl_rec="";
-            for (int k = 0; k < n; k++) {
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        float aux=D[i][j];
-                        float aux2=D[i][k];
-                        float aux3=D[k][j];
-                        float aux4=aux2+aux3;
-                        float res=Math.min(aux,aux4);
-                        if(aux!=aux4)
-                        {
-                            if(res==aux4)
-                            {
-                                enl_rec="";
-                                aux_enlaces[i][j]=k+"";
-                                enlaces[i][j]=enlaces(i,k,aux_enlaces,enl_rec)+(k+1);
-                            }
-                        }
-                        D[i][j]=(long) res;
-                    }
-                }
-            }
-
-            String cadena="";
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    cadena+=D[i][j]+" ";
-                }
-                cadena+="\n";
-            }
-
-            String enlacesres="";
-            for (int i = 0; i <n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if(i!=j)
+                    float aux=D[i][j];
+                    float aux2=D[i][k];
+                    float aux3=D[k][j];
+                    float aux4=aux2+aux3;
+                    float res=Math.min(aux,aux4);
+                    if(aux!=aux4)
                     {
-                        if(enlaces[i][j].equals("")==true)
+                        if(res==aux4)
                         {
-                            enlacesres+=" De ( "+(i)+" a "+(j)+" ) = "+"( "+(i)+" , "+(j)+" )"+"\n";
-                            System.out.println(D[i][j]+"");
+                            enl_rec="";
+                            aux_enlaces[i][j]=k+"";
+                            enlaces[i][j]=enlaces(i,k,aux_enlaces,enl_rec)+(k);
                         }
-                        else
-                            enlacesres+=" De ( "+(i)+" a "+(j)+" ) = ( "+(i)+" , "+enlaces[i][j]+" , "+(j)+")\n";
-                        System.out.println(D[i][j]+"");
                     }
+                    D[i][j]=(long) res;
                 }
             }
-
-            return "las distancias minimas entre nodos son: \n"+cadena+"\nlos caminos minimos entre nodos son:\n"+enlacesres+
-                    "\nresultado: "+resultado;
         }
 
-        public String enlaces(int i,int k,String[][] aux_enlaces,String enl_rec)
+        String cadena="";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cadena+=D[i][j]+" ";
+            }
+            cadena+="\n";
+        }
+
+        String enlacesres="";
+        for (int i = 0; i <n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i!=j)
+                {
+                    if(enlaces[i][j].equals("")==true)
+                    {
+                        enlacesres+=" De ( "+(i)+" a "+(j)+" ) = "+"( "+(i)+" , "+(j)+" ) = "+D[i][j]+"\n";
+                    }
+                    else
+                        enlacesres+=" De ( "+(i)+" a "+(j)+" ) = ( "+(i)+" , "+enlaces[i][j]+" , "+(j)+")= "+D[i][j]+"\n";
+                }
+            }
+        }
+
+        return "las distancias minimas entre nodos son: \n"+cadena+"\nlos caminos minimos entre nodosson:\n"+enlacesres;
+    }
+
+    public String enlaces(int i,int k,String[][] aux_enlaces,String enl_rec)
+    {
+        if(aux_enlaces[i][k].equals("")==true)
         {
-            if(aux_enlaces[i][k].equals("")==true)
-            {
-                return "";
-            }
-            else
-            {
-                enl_rec+=enlaces(i,Integer.parseInt(aux_enlaces[i][k].toString()),aux_enlaces,enl_rec)+(Integer.parseInt(aux_enlaces[i][k].toString())+1)+" , ";
-                return enl_rec;
-            }
+            return "";
         }
+        else
+        {
+            enl_rec+=enlaces(i,Integer.parseInt(aux_enlaces[i][k].toString()),aux_enlaces,enl_rec)+(Integer.parseInt(aux_enlaces[i][k].toString())+1)+" , ";
+
+            return enl_rec;
+        }
+    }
 
 }
 
--- src/prueba.java
+++ src/prueba.java
@@ -1,11 +1,17 @@
 public class prueba {
     static AlgoritmoFloyd algoritmo = new AlgoritmoFloyd();
     public static void main(String[] args) {
-        long[][] m = new long[2][2];
-        m[0][0] = 100;
-        m[0][1] = 150;
-        m[1][0] = 0;
-        m[1][1] = 50;
+        long[][] m = new long[3][3];
+        m[0][0] = 0;
+        m[0][1] = 100;
+        m[0][2] = 200;
+        m[1][0] = 100;
+        m[1][1] = 0;
+        m[1][2] = 50;
+        m[2][0] = 200;
+        m[2][1] = 50;
+        m[2][2] = 0;
+
 
         System.out.println(algoritmo.floyd(m));
     }
